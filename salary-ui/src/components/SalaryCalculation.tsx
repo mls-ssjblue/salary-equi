38fdf110e6d239f41818a75a12298f02cfe0bfa0
Susheel <mlsusheel@gmail.com>
2021-07-29 00:38:45 +0800

    api WIP

--- salary-ui/src/components/SalaryCalculation.tsx
+++ salary-ui/src/components/SalaryCalculation.tsx
@@ -3,30 +3,58 @@ import '../Salary.css'
 import {useState} from "react";
 
 export const SalaryCalculation = () => {
-    const tax = useState(0);
+    const [salary, setSalary] = useState(0);
+    const [country, setCountry] = useState('Singapore');
+    const [tax, setTax] = useState(0);
     return (
         <StyledStage>
-            <div className="salary">
-                <div className="field">
-                    <span>Country: </span>
-                    <select>
-                        <option>UK</option>
-                        <option>Singapore</option>
-                    </select>
+            <form>
+                <div className="salary">
+                    <div className="field">
+                        <span>Country: </span>
+                        <select onChange={(e) =>
+                            setCountry(e.target.value)}>
+                            <option>UK</option>
+                            <option>Singapore</option>
+                        </select>
+                    </div>
+                    <div className="field">
+                        <span>Salary: </span>
+                        <input onChange={e => setSalary(+e.target.value)}/>
+                    </div>
+                    <button onClick={e => setTax(calculateTax(country, salary))}/>
+                    <div className="tax">Tax: {tax}</div>
+
                 </div>
-                <div className="field">
-                    <span>Salary: </span>
-                    <input/>
-                </div>
-                <div className="tax">Tax: {tax}</div>
-            </div>
+            </form>
         </StyledStage>
     )
 }
 
+
+const calculateTax = (country :string, salary :number) : number => {
+    let tax :number;
+    fetch('http:localhost:123',{
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        "body": JSON.stringify({
+            country,
+            salary
+        })
+    }).then(response=>response.json())
+        .then(response=>tax=response.tax);
+    return tax;
+}
 const StyledStage = styled.div`
     margin:20px;
     padding: 10px;
     text-align: center;
     display:flex;
 `;
+
+interface ApiResponse {
+    tax: number
+}
