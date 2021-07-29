cd185dbd8645945c3106ca5a8e89db8d04462a20
mlssjblue <mlsusheel@gmail.com>
2021-07-29 09:10:08 +0800

    Fix get request, add proxy

--- salary-ui/src/components/SalaryCalculation.tsx
+++ salary-ui/src/components/SalaryCalculation.tsx
@@ -1,50 +1,43 @@
 import styled from 'styled-components'
 import '../Salary.css'
-import {useState} from "react";
+import {useState} from "react"
 
 export const SalaryCalculation = () => {
-    const [salary, setSalary] = useState(0);
-    const [country, setCountry] = useState('Singapore');
-    const [tax, setTax] = useState(0);
+    const [salary, setSalary] = useState(0)
+    const [country, setCountry] = useState('Singapore')
+    const [tax, setTax] = useState(0)
 
 
-    const calculateTax = (country :string, salary :number)  => {
-        fetch('http:localhost:123',{
-            "method": "POST",
+    const calculateTax = (country: string, salary: number) => {
+        fetch(`http:localhost:8080/v1/taxPayable?country=${country}&salary=${salary}`, {
+            "method": "GET",
             "headers": {
                 "content-type": "application/json",
                 "accept": "application/json"
-            },
-            "body": JSON.stringify({
-                country,
-                salary
-            })
-        }).then(response=>response.json())
-            .then(response=>setTax(response.tax));
-        ;
+            }
+        }).then(response => response.json())
+            .then(response => setTax(response.tax));
 
-    };
+    }
     return (
         <StyledStage>
-            <form>
-                <div className="salary">
-                    <div className="field">
-                        <span>Country: </span>
-                        <select onChange={(e) =>
-                            setCountry(e.target.value)}>
-                            <option>UK</option>
-                            <option>Singapore</option>
-                        </select>
-                    </div>
-                    <div className="field">
-                        <span>Salary: </span>
-                        <input onChange={e => setSalary(+e.target.value)}/>
-                    </div>
-                    <button onClick={e => calculateTax(country, salary)}/>
-                    <div className="tax">Tax: {tax}</div>
-
+            <div className="salary">
+                <div className="field">
+                    <span>Country: </span>
+                    <select onChange={(e) =>
+                        setCountry(e.target.value)}>
+                        <option>UK</option>
+                        <option>Singapore</option>
+                    </select>
                 </div>
-            </form>
+                <div className="field">
+                    <span>Salary: </span>
+                    <input onChange={e => setSalary(+e.target.value)}/>
+                </div>
+                <button onClick={() => calculateTax(country, salary)}> Calculate</button>
+                <div className="tax">Tax: {tax}</div>
+
+            </div>
         </StyledStage>
     )
 }
@@ -55,7 +48,7 @@ const StyledStage = styled.div`
     padding: 10px;
     text-align: center;
     display:flex;
-`;
+`
 
 interface ApiResponse {
     tax: number
