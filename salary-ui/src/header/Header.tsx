6d521f9dc3000fc73fb28b15d2c049b7395d419b
mlssjblue <mlsusheel@gmail.com>
2021-08-08 22:38:43 +0800

    Style header colors and font

--- salary-ui/src/header/Header.tsx
+++ salary-ui/src/header/Header.tsx
@@ -4,30 +4,8 @@ import Typography from "@material-ui/core/Typography"
 
 export const Header: React.FC = () => {
     return (
-        <StyledHeader>
-            SALARY EQUIVALENCE
-        </StyledHeader>
+        <div className="header">
+            <h1>Salary Equivalence</h1>
+        </div>
     )
 }
-
-const StyledHeader = styled.div`
-  padding: 5px;
-  background: #87D691FF;
-   font-size: 3rem;
-  letter-spacing: 2px;
-  justify-content: center;
-  overflow-x: auto;
-  width: 100vw;
-`
-//
-// const StyledHeading = styled.h1`
-//   max-width: 650px;
-//   color: white;
-//   font-size: 3.5rem;
-//   letter-spacing: 4px;
-//   overflow: hidden; /* Ensures the content is not revealed until the animation */
-//   white-space: nowrap; /* Keeps the content on a single line */
-//   margin: 0 auto; /* Gives that scrolling effect as the typing happens */
-//   background: #87D691FF;
-// `
-
