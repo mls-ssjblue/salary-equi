c131e9ca52d3976d06a13cf7a16a4403b16a08fc
mlssjblue <mlsusheel@gmail.com>
2021-08-09 20:24:11 +0800

    Highlight current stage

--- salary-ui/src/components/SalaryContainer.tsx
+++ salary-ui/src/components/SalaryContainer.tsx
@@ -1,10 +1,12 @@
 import {SalaryCalculation} from './SalaryCalculation'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 export const SalaryContainer = () => {
 
     useEffect(() => {
     }, [])
+
+    const [activeStage, setActiveStage] = useState(1)
 
     return (
         <div className="slider">
@@ -14,8 +16,8 @@ export const SalaryContainer = () => {
                 <SalaryCalculation id="slide-2"/>
             </div>
             <div className="navigation-panel">
-                <a href="#slide-1">1</a>
-                <a href="#slide-2">2</a>
+                <a href="#slide-1" className={activeStage==1?'active':''} onClick={()=>setActiveStage(1)}>1</a>
+                <a href="#slide-2" className={activeStage==2?'active':''} onClick={()=>setActiveStage(2)}>2</a>
             </div>
         </div>
     )
